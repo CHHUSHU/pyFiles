9c1e3c5325edc3dcfe09fcf6991d8dce39de95d2
Chushu <39640111+CHHUSHU@users.noreply.github.com>
2021-07-26 14:24:49 +0800

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+def ordered_dict():
+    d = OrderedDict()
+    d['foo'] = 1
+    d['bar'] = 2
+    d['spam'] = 3
+    d['grok'] = 4
+
+    for key in d:
+        print(key, d[key])
+        
+
+import json
+json.dumps(d)
